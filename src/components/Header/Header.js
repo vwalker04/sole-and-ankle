2143a41d712f90446d2a3b05dfa8dd98d2519a2f
Vaughn Walker <vwalk04@gmail.com>
2024-02-02 16:00:18 -0500

    Update style for header group

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,48 +1,62 @@
 import React from 'react';
 import styled from 'styled-components/macro';
 
-import { COLORS, WEIGHTS } from '../../constants';
+import {COLORS, WEIGHTS} from '../../constants';
 import Logo from '../Logo';
 import SuperHeader from '../SuperHeader';
 
 const Header = () => {
-  // Our site features two visual headers, but they should be
-  // grouped semantically as a single header.
-  return (
-    <header>
-      <SuperHeader />
-      <MainHeader>
-        <Logo />
-        <Nav>
-          <NavLink href="/sale">Sale</NavLink>
-          <NavLink href="/new">New&nbsp;Releases</NavLink>
-          <NavLink href="/men">Men</NavLink>
-          <NavLink href="/women">Women</NavLink>
-          <NavLink href="/kids">Kids</NavLink>
-          <NavLink href="/collections">Collections</NavLink>
-        </Nav>
-      </MainHeader>
-    </header>
-  );
+	// Our site features two visual headers, but they should be
+	// grouped semantically as a single header.
+	return (
+		<header>
+			<SuperHeader/>
+			<MainHeader>
+				<Side>
+					<Logo/>
+				</Side>
+				<Nav>
+					<NavLink href="/sale">Sale</NavLink>
+					<NavLink href="/new">New&nbsp;Releases</NavLink>
+					<NavLink href="/men">Men</NavLink>
+					<NavLink href="/women">Women</NavLink>
+					<NavLink href="/kids">Kids</NavLink>
+					<NavLink href="/collections">Collections</NavLink>
+				</Nav>
+				<Side/>
+			</MainHeader>
+		</header>
+	);
 };
 
 const MainHeader = styled.div`
-  padding: 0 32px;
-  border-bottom: 1px solid ${COLORS.gray[300]};
+    padding: 18px 32px;
+    border-bottom: 1px solid ${COLORS.gray[300]};
+    min-height: 72px;
+	display: flex;
+	align-items: baseline;
 `;
 
-const Nav = styled.nav``;
+const Side = styled.div`
+    flex: 1;
+`
+
+const Nav = styled.nav`
+    display: flex;
+	gap: 48px;
+	margin: 0 48px;
+`;
 
 const NavLink = styled.a`
-  font-size: 1.125rem;
-  text-transform: uppercase;
-  text-decoration: none;
-  color: ${COLORS.gray[900]};
-  font-weight: ${WEIGHTS.medium};
+    font-size: 1.125rem;
+    text-transform: uppercase;
+    text-decoration: none;
+    color: ${COLORS.gray[900]};
+    font-weight: ${WEIGHTS.medium};
 
-  &:first-of-type {
-    color: ${COLORS.secondary};
-  }
+    &:first-of-type {
+        color: ${COLORS.secondary};
+    }
 `;
 
 export default Header;
