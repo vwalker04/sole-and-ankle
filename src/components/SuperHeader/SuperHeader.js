394b30eb0354106304986b52348d78553e887471
Vaughn Walker <vwalk04@gmail.com>
2024-02-02 10:35:22 -0500

    Style SuperHeader

--- src/components/SuperHeader/SuperHeader.js
+++ src/components/SuperHeader/SuperHeader.js
@@ -1,45 +1,50 @@
 import React from 'react';
 import styled from 'styled-components';
 
-import { COLORS } from '../../constants';
+import {COLORS} from '../../constants';
 
 import SearchInput from '../SearchInput';
 import UnstyledButton from '../UnstyledButton';
 import Icon from '../Icon';
 
 const SuperHeader = () => {
-  return (
-    <Wrapper>
-      <MarketingMessage>
-        Free shipping on domestic orders over $75!
-      </MarketingMessage>
-      <SearchInput />
-      <HelpLink href="/help">Help</HelpLink>
-      <UnstyledButton>
-        <Icon id="shopping-bag" strokeWidth={1} />
-      </UnstyledButton>
-    </Wrapper>
-  );
+	return (
+		<Wrapper>
+			<MarketingMessage>
+				Free shipping on domestic orders over $75!
+			</MarketingMessage>
+			<SearchInput/>
+			<HelpLink href="/help">Help</HelpLink>
+			<UnstyledButton>
+				<Icon id="shopping-bag" strokeWidth={1}/>
+			</UnstyledButton>
+		</Wrapper>
+	);
 };
 
 const Wrapper = styled.div`
-  font-size: 0.875rem;
-  color: ${COLORS.gray[300]};
-  background-color: ${COLORS.gray[900]};
+    font-size: 0.875rem;
+    color: ${COLORS.gray[300]};
+    background-color: ${COLORS.gray[900]};
+    display: flex;
+	padding: 12px 32px;
 `;
 
 const MarketingMessage = styled.span`
-  color: ${COLORS.white};
+    color: ${COLORS.white};
+	margin-right: auto;
 `;
 
 const HelpLink = styled.a`
-  color: inherit;
-  text-decoration: none;
-  outline-offset: 2px;
+    color: inherit;
+    text-decoration: none;
+    outline-offset: 2px;
+	padding-left: 24px;
+	padding-right: 24px;
 
-  &:not(:focus-visible) {
-    outline: none;
-  }
+    &:not(:focus-visible) {
+        outline: none;
+    }
 `;
 
 export default SuperHeader;
