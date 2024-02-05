53bb73676fa598b75d42b190670a4efeed5593df
Vaughn Walker <vwalk04@gmail.com>
2024-02-05 10:52:58 -0500

    Complete exercise with updated approach

--- src/components/ShoeCard/ShoeCard.js
+++ src/components/ShoeCard/ShoeCard.js
@@ -1,86 +1,133 @@
 import React from 'react';
 import styled from 'styled-components/macro';
 
-import { COLORS, WEIGHTS } from '../../constants';
-import { formatPrice, pluralize, isNewShoe } from '../../utils';
+import {COLORS, WEIGHTS} from '../../constants';
+import {formatPrice, isNewShoe, pluralize} from '../../utils';
 import Spacer from '../Spacer';
 
 const ShoeCard = ({
-  slug,
-  name,
-  imageSrc,
-  price,
-  salePrice,
-  releaseDate,
-  numOfColors,
-}) => {
-  // There are 3 variants possible, based on the props:
-  //   - new-release
-  //   - on-sale
-  //   - default
-  //
-  // Any shoe released in the last month will be considered
-  // `new-release`. Any shoe with a `salePrice` will be
-  // on-sale. In theory, it is possible for a shoe to be
-  // both on-sale and new-release, but in this case, `on-sale`
-  // will triumph and be the variant used.
-  // prettier-ignore
-  const variant = typeof salePrice === 'number'
-    ? 'on-sale'
-    : isNewShoe(releaseDate)
-      ? 'new-release'
-      : 'default'
+					  slug,
+					  name,
+					  imageSrc,
+					  price,
+					  salePrice,
+					  releaseDate,
+					  numOfColors,
+				  }) => {
+	// There are 3 variants possible, based on the props:
+	//   - new-release
+	//   - on-sale
+	//   - default
+	//
+	// Any shoe released in the last month will be considered
+	// `new-release`. Any shoe with a `salePrice` will be
+	// on-sale. In theory, it is possible for a shoe to be
+	// both on-sale and new-release, but in this case, `on-sale`
+	// will triumph and be the variant used.
+	// prettier-ignore
+	const variant = typeof salePrice === 'number'
+		? 'on-sale'
+		: isNewShoe(releaseDate)
+			? 'new-release'
+			: 'default'
 
-  return (
-    <Link href={`/shoe/${slug}`}>
-      <Wrapper>
-        <ImageWrapper>
-          <Image alt="" src={imageSrc} />
-        </ImageWrapper>
-        <Spacer size={12} />
-        <Row>
-          <Name>{name}</Name>
-          <Price>{formatPrice(price)}</Price>
-        </Row>
-        <Row>
-          <ColorInfo>{pluralize('Color', numOfColors)}</ColorInfo>
-        </Row>
-      </Wrapper>
-    </Link>
-  );
+	return (
+		<Link href={`/shoe/${slug}`}>
+			<Wrapper>
+				<ImageWrapper>
+					{variant === 'on-sale' && <SaleFlag>Sale</SaleFlag>}
+					{variant === 'new-release' && <NewReleaseFlag>Just Released!</NewReleaseFlag>}
+					<Image alt="" src={imageSrc}/>
+				</ImageWrapper>
+				<Spacer size={12}/>
+				<DetailWrapper>
+					<Row>
+						<Name>{name}</Name>
+						<ColorInfo>{pluralize('Color', numOfColors)}</ColorInfo>
+					</Row>
+					<Row>
+						{salePrice ?
+							<>
+								<Price onSale>{formatPrice(price)}</Price>
+								<SalePrice>{formatPrice(salePrice)}</SalePrice>
+							</>
+							:
+							<Price>{formatPrice(price)}</Price>
+						}
+					</Row>
+				</DetailWrapper>
+			</Wrapper>
+		</Link>
+	);
 };
 
 const Link = styled.a`
-  text-decoration: none;
-  color: inherit;
+    text-decoration: none;
+    color: inherit;
 `;
 
-const Wrapper = styled.article``;
+const Wrapper = styled.article`
+    margin-bottom: 30px;
+`;
 
 const ImageWrapper = styled.div`
-  position: relative;
+    position: relative;
 `;
 
-const Image = styled.img``;
+const Adornment = styled.div`
+    position: absolute;
+    top: 12px;
+    right: -4px;
+    border-radius: 2px;
+    padding: 0 10px;
+    height: 32px;
+    line-height: 32px;
+    color: white;
+	font-size: ${14 / 16}rem;
+    font-weight: ${WEIGHTS.bold};
+`
+
+const SaleFlag = styled(Adornment)`
+	background-color: ${COLORS.primary};
+`
+
+const NewReleaseFlag = styled(Adornment)`
+    background-color: ${COLORS.secondary};
+`
+
+const DetailWrapper = styled.div`
+    display: flex;
+    justify-content: space-between;
+`
+
+const Image = styled.img`
+	width: 100%;
+    border-radius: 16px 16px 4px 4px;
+`;
 
 const Row = styled.div`
-  font-size: 1rem;
+    font-size: 1rem;
+    display: flex;
+    flex-direction: column;
 `;
 
 const Name = styled.h3`
-  font-weight: ${WEIGHTS.medium};
-  color: ${COLORS.gray[900]};
+    font-weight: ${WEIGHTS.medium};
+    color: ${COLORS.gray[900]};
 `;
 
-const Price = styled.span``;
+const Price = styled.span`
+    color: ${props => props.onSale ? COLORS.gray["700"] : 'inherit'};
+    text-decoration: ${props => props.onSale ? 'line-through' : 'none'};
+`;
 
 const ColorInfo = styled.p`
-  color: ${COLORS.gray[700]};
+    color: ${COLORS.gray[700]};
 `;
 
 const SalePrice = styled.span`
-  font-weight: ${WEIGHTS.medium};
-  color: ${COLORS.primary};
+    font-weight: ${WEIGHTS.medium};
+    color: ${COLORS.primary};
 `;
 
 export default ShoeCard;
--- src/components/ShoeIndex/ShoeIndex.js
+++ src/components/ShoeIndex/ShoeIndex.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from 'styled-components/macro';
 
-import { WEIGHTS } from '../../constants';
+import {WEIGHTS} from '../../constants';
 
 import Breadcrumbs from '../Breadcrumbs';
 import Select from '../Select';
@@ -9,50 +9,64 @@ import Spacer from '../Spacer';
 import ShoeSidebar from '../ShoeSidebar';
 import ShoeGrid from '../ShoeGrid';
 
-const ShoeIndex = ({ sortId, setSortId }) => {
-  return (
-    <Wrapper>
-      <MainColumn>
-        <Header>
-          <Title>Running</Title>
-          <Select
-            label="Sort"
-            value={sortId}
-            onChange={(ev) => setSortId(ev.target.value)}
-          >
-            <option value="newest">Newest Releases</option>
-            <option value="price">Price</option>
-          </Select>
-        </Header>
-        <Spacer size={34} />
-        <ShoeGrid />
-      </MainColumn>
-      <LeftColumn>
-        <Breadcrumbs>
-          <Breadcrumbs.Crumb href="/">Home</Breadcrumbs.Crumb>
-          <Breadcrumbs.Crumb href="/sale">Sale</Breadcrumbs.Crumb>
-          <Breadcrumbs.Crumb href="/sale/shoes">
-            Shoes
-          </Breadcrumbs.Crumb>
-        </Breadcrumbs>
-        <Spacer size={42} />
-        <ShoeSidebar />
-      </LeftColumn>
-    </Wrapper>
-  );
+const ShoeIndex = ({sortId, setSortId}) => {
+	return (
+		<Wrapper>
+			<MainColumn>
+				<Header>
+					<Title>Running</Title>
+					<Select
+						label="Sort"
+						value={sortId}
+						onChange={(ev) => setSortId(ev.target.value)}
+					>
+						<option value="newest">Newest Releases</option>
+						<option value="price">Price</option>
+					</Select>
+				</Header>
+				<Spacer size={34}/>
+				<ShoeGrid/>
+			</MainColumn>
+			<LeftColumn>
+				<Breadcrumbs>
+					<Breadcrumbs.Crumb href="/">Home</Breadcrumbs.Crumb>
+					<Breadcrumbs.Crumb href="/sale">Sale</Breadcrumbs.Crumb>
+					<Breadcrumbs.Crumb href="/sale/shoes">
+						Shoes
+					</Breadcrumbs.Crumb>
+				</Breadcrumbs>
+				<Spacer size={42}/>
+				<ShoeSidebar/>
+			</LeftColumn>
+		</Wrapper>
+	);
 };
 
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+    display: flex;
+    flex-direction: row-reverse;
+    gap: 32px;
+	align-items: baseline;
+`;
 
-const LeftColumn = styled.div``;
+const LeftColumn = styled.div`
+    flex-basis: 248px;
+`;
 
-const MainColumn = styled.div``;
+const MainColumn = styled.div`
+    flex: 1;
+`;
 
-const Header = styled.header``;
+const Header = styled.header`
+    display: flex;
+    justify-content: space-between;
+	align-items: baseline;
+`;
 
 const Title = styled.h2`
-  font-size: 1.5rem;
-  font-weight: ${WEIGHTS.medium};
+    font-size: 1.5rem;
+    font-weight: ${WEIGHTS.medium};
+    //padding-top: 4px;
 `;
 
 export default ShoeIndex;
